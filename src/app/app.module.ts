5703f1e4d39662d3361ca1154860fd44e78eccbf
fredowell <45028136+fredowell@users.noreply.github.com>
2020-11-11 16:10:57 +0800

    Added Homepage
    Added homepage component, adjusted routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
