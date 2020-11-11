7e1bb01cfa65642a1239fb2da4bdb80ca2a8e597
fredowell <45028136+fredowell@users.noreply.github.com>
2020-11-11 22:44:33 +0800

    A really big update
    Added:
    Sign in
    login
    home page
    FAQ
    products
    and updated the css and routing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const appRoutes: Routes = [
-    {path: '', component: HomepageComponent, pathMatch: 'full'},
-    {path:'login', component: LoginPageComponent}
+    {path:'', redirectTo:'/home', pathMatch: 'full'},
+    {path:'home', component: HomepageComponent},
+    {path:'login', component: LoginPageComponent},
+    {path:'sign-up', component: SignupPageComponent},
+    {path:'about-us', component: AboutUsComponent},
+    {path:'products', component: ProductsPageComponent},
+    {path: 'FAQs', component: FaqPageComponent}
 ]
 
 @NgModule(
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     HeaderComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AboutUsComponent,
+    ProductsPageComponent,
+    SignupPageComponent,
+    FaqPageComponent
   ],
   imports: [
     BrowserModule,
